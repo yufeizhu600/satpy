3a841922692a1f95e2734dafdd650a7814b3d677
Yufei Zhu <yufei.zhu@ec.gc.ca>
2024-02-12 12:45:46 -0500

    fix mci_derived_chla band order, rename the variable according to SNAP implementation

--- satpy/composites/olci.py
+++ satpy/composites/olci.py
@@ -16,17 +16,22 @@
 # You should have received a copy of the GNU General Public License along with
 # satpy.  If not, see <http://www.gnu.org/licenses/>.
 """Composite classes for the Sentinel-3 OLCI instrument."""
-
+import numpy as np
 import logging
 
-from satpy.composites import CompositorBase
+from satpy.composites import CompositeBase
+from satpy.dataset import combine_metadata
+import dask.array as da
 
 LOG = logging.getLogger(__name__)
 
 
-class MCIDerivedChla(CompositorBase):
-    """MCI compositor 
-    (MCI <= 0 or  MCI_slope > -0.15) and (Chlorophyll >= 0 and Chlorophyll <= 200)      
+class MCIDerivedChla(CompositeBase):
+    """Maximum Chlorophyll Index derived Chlorophyll compositor.
+    MCI and MCI_slope reference:
+
+    https://github.com/senbox-org/optical-toolbox/tree/master/opttbx-s2msi-mci/src/main/java/eu/esa/opt/processor/mci
+    (MCI <= 0 or  MCI_slope > -0.15) and (Chlorophyll >= 0 and Chlorophyll <= 200)
     """
 
     def __call__(self, projectables, nonprojectables=None, **attrs):
@@ -35,70 +40,41 @@ class MCIDerivedChla(CompositorBase):
 
         projectables = self.match_data_arrays(projectables)
         info = combine_metadata(*projectables)
-        info['name'] = self.attrs['name']
+        info["name"] = self.attrs["name"]
         info.update(self.attrs)  # attrs from YAML/__init__
         info.update(attrs)  # overwriting of DataID properties
 
-        proj.attrs = info
-        # return proj
+        # get wavelength and convert units, "where for OLCI, λ1, λ2 and λ3 are centered at 681, 708, and 753 nm respectively."
+        upper_lambda = np.floor(projectables[0].attrs["wavelength"].central * 1000)
+        signal_lambda = np.floor(projectables[1].attrs["wavelength"].central * 1000)
+        lower_lambda = np.floor(projectables[2].attrs["wavelength"].central * 1000)
 
-        lambda_1 = projectables[0].attrs["wavelength"].central * 1000
-        lambda_2 = projectables[1].attrs["wavelength"].central * 1000
-        lambda_3 = projectables[2].attrs["wavelength"].central * 1000
+        # set numerator and denominator
+        num = signal_lambda - lower_lambda
+        denom = upper_lambda - lower_lambda
 
-        L1, L2, L3  = self.match_data_arrays(projectables)
-        
-        MCI = L2 - L1 - (lambda_2 - lambda_1) / (lambda_3 - lambda_1) * (L3 -L1)
+        # inverse wavelength delta needed for baseline slope calculation
+        inverse_delta = 1.0 / denom
 
-        MCI_slope = (L3 - L1) / (lambda_3 - lambda_1)
+        # wavelength factor
+        lambda_factor = num / denom
 
-        Chla = 6.166 * MCI + 6.347
+        upper, peak, lower = self.match_data_arrays(projectables)
 
-        Chla = da.clip(Chla, 0, 200)
+        mci = peak - lower - (upper - lower) * lambda_factor
 
-        Chla = Chla.where(MCI <= 0)
+        mci_slope = (upper - lower) * inverse_delta
 
-        Chla = chla.where(MCI_slope > -0.15)
+        chla = (6.166 * mci + 6.347).clip(0, 200)
 
-        Chla.attrs = info
+        # chla = 6.166 * mci + 6.347
 
-        return Chla
+        # chla = chla.clip(0, 200)
 
+        chla.where(mci <= 0, np.NaN)
 
+        chla.where(mci_slope <= -0.15, np.NaN)
 
-class SimulatedGreen(GenericCompositor):
-    """A single-band dataset resembling a Green (0.55 µm) band.
+        chla.attrs = info
 
-    This compositor creates a single band product by combining three
-    other bands in various amounts. The general formula with
-    dependencies (d) and fractions (f) is::
-
-        result = d1 * f1 + d2 * f2 + d3 * f3
-
-    See the `fractions` keyword argument for more information.
-    Common used fractions for ABI data with C01, C02, and C03 inputs include:
-
-    - SatPy default (historical): (0.465, 0.465, 0.07)
-    - `CIMSS (Kaba) <https://agupubs.onlinelibrary.wiley.com/doi/10.1029/2018EA000379>`_: (0.45, 0.45, 0.10)
-    - `EDC <http://edc.occ-data.org/goes16/python/>`_: (0.45706946, 0.48358168, 0.06038137)
-
-    """
-
-    def __init__(self, name, fractions=(0.465, 0.465, 0.07), **kwargs):
-        """Initialize fractions for input channels.
-
-        Args:
-            name (str): Name of this composite
-            fractions (iterable): Fractions of each input band to include in the result.
-
-        """
-        self.fractions = fractions
-        super(SimulatedGreen, self).__init__(name, **kwargs)
-
-    def __call__(self, projectables, optional_datasets=None, **attrs):
-        """Generate the single band composite."""
-        c01, c02, c03 = self.match_data_arrays(projectables)
-        res = c01 * self.fractions[0] + c02 * self.fractions[1] + c03 * self.fractions[2]
-        res.attrs = c03.attrs.copy()
-        return super(SimulatedGreen, self).__call__((res,), **attrs)
-
+        return chla
--- satpy/tests/reader_tests/test_olci_nc.py
+++ satpy/tests/reader_tests/test_olci_nc.py
@@ -137,6 +137,34 @@ class TestOLCIReader(unittest.TestCase):
         expected = np.array([True] + [False] * 29).reshape(5, 6)
         np.testing.assert_array_equal(res.values, expected)
 
+
+    @mock.patch("xarray.open_dataset")
+    def test_get_mask_l1b(self, mocked_dataset):
+        """Test reading mask datasets from L1B products."""
+        import numpy as np
+        import xarray as xr
+
+        from satpy.readers.olci_nc import NCOLCI1B
+        from satpy.tests.utils import make_dataid
+        mocked_dataset.return_value = xr.Dataset({"quality_flags": (["rows", "columns"],
+                                                           np.array([1 << (x % 32) for x in range(35)]).reshape(5, 7))},
+                                                 coords={"rows": np.arange(5),
+                                                         "columns": np.arange(7)})
+        ds_id = make_dataid(name="mask")
+        filename_info = {"mission_id": "S3A", "dataset_name": "mask", "start_time": 0, "end_time": 0}
+        test = NCOLCI1B("somedir/somefile.nc", filename_info, "c")
+        res = test.get_dataset(ds_id, {"nc_key": "quality_flags"})
+        assert res.dtype == np.dtype("bool")
+
+        expected = np.array([[False, False, False, False, False, False, False],
+                             [False, False, False, False, False, False, False],
+                             [False, False, False, False, False, False, False],
+                             [True, True, True, True, True, True, True],
+                             [True, False, True, True, False, False, False]])
+
+        np.testing.assert_array_equal(res.values, expected)
+
+
     @mock.patch("xarray.open_dataset")
     def test_olci_angles(self, mocked_dataset):
         """Test reading datasets."""
@@ -241,7 +269,7 @@ class TestOLCIReader(unittest.TestCase):
         assert res.values[-1, -1] == 1e29
 
 
-class TestBitFlags(unittest.TestCase):
+class TestL2BitFlags(unittest.TestCase):
     """Test the bitflag reading."""
 
     def test_bitflags(self):
@@ -274,3 +302,46 @@ class TestBitFlags(unittest.TestCase):
                              False, False,  True,  True, False, False, True,
                              False])
         assert all(mask == expected)
+
+
+class TestL1bBitFlags(unittest.TestCase):
+    """Test the bitflag reading."""
+
+    def test_bitflags(self):
+        """Test the BitFlags class."""
+        from functools import reduce
+
+        import numpy as np
+
+        from satpy.readers.olci_nc import BitFlags
+
+
+        L1B_QUALITY_FLAGS = ['saturated@Oa21', 'saturated@Oa20', 'saturated@Oa19', 'saturated@Oa18',
+                             'saturated@Oa17', 'saturated@Oa16', 'saturated@Oa15', 'saturated@Oa14',
+                             'saturated@Oa13', 'saturated@Oa12', 'saturated@Oa11', 'saturated@Oa10',
+                             'saturated@Oa09', 'saturated@Oa08', 'saturated@Oa07', 'saturated@Oa06',
+                             'saturated@Oa05', 'saturated@Oa04', 'saturated@Oa03', 'saturated@Oa02',
+                             'saturated@Oa01', 'dubious', 'sun-glint_risk', 'duplicated',
+                             'cosmetic', 'invalid', 'straylight_risk', 'bright',
+                             'tidal_region', 'fresh_inland_water', 'coastline', 'land']
+
+        DEFAULT_L1B_MASK_ITEMS = ['dubious', 'sun-glint_risk', 'duplicated', 'cosmetic', 'invalid',
+                                  'straylight_risk', 'bright', 'tidal_region', 'coastline', 'land']
+
+        bits = np.array([1 << x for x in range(len(L1B_QUALITY_FLAGS))])
+
+        bflags = BitFlags(bits, flag_list=L1B_QUALITY_FLAGS)
+
+        mask = reduce(np.logical_or, [bflags[item] for item in DEFAULT_L1B_MASK_ITEMS])
+
+        expected = np.array([False, False, False, False,
+                             False, False, False, False,
+                             False, False, False, False,
+                             False, False, False, False,
+                             False, False, False, False,
+                             False, True,  True,  True,
+                             True,  True,  True,  True,
+                             True,  False, True,  True,
+                            ])
+        assert all(mask == expected)
+
